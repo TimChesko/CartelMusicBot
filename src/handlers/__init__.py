17fed98bd6ef1c88a6dac47a9cac3ed0ddbbf517
Tima <timofey.lazarev@gmail.com>
2023-06-12 19:47:29 +0300

    Add flag typing via all handlers and add example  flag into start

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -1,7 +1,10 @@
 from aiogram import Router
 
 from . import private
+from ..middlewares.chat_actions import ChatActions
 
 router = Router()
+
+router.message.middleware(ChatActions())
 
 router.include_router(private.router)
--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -1,9 +1,13 @@
+import asyncio
+
 from aiogram import Router
 from aiogram.filters import CommandStart
+from aiogram.types import Message
 
 router = Router()
 
 
-@router.message(CommandStart())
-async def cmd_start():
-    pass
+@router.message(CommandStart(), flags={'chat_action': 'typing'})
+async def cmd_start(msg: Message):
+    await asyncio.sleep(5)
+    await msg.answer("Привет")
