6f83e5a7dc8882156cd827b44f6833e1afb02b57
Tima <timofey.lazarev@gmail.com>
2023-06-13 10:55:09 +0300

    Example of adding dialogs

--- src/handlers/private/user/start.py
+++ src/handlers/private/user/start.py
@@ -4,7 +4,13 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from src.service.diologs_collector import DialogsCollector
+
 router = Router()
+
+dialogs = DialogsCollector()
+dialogs.include_dialogs(123, "124")
+dialogs.include_dialog("TEST")
 
 
 @router.message(CommandStart(), flags={'chat_action': 'typing'})
