6117201fbfa8ee8d8e7f572834bcdc873ac2f23d
Tima <timofey.lazarev@gmail.com>
2023-06-14 10:31:36 +0300

    Some formating

--- src/database/process.py
+++ src/database/process.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.orm import sessionmaker
 
 from src.models.tables import Base
 
