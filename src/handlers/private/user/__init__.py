874ccb6aeca32c2f6dd344c4e0c95ac1a9c1ac68
timchesko <i@tlazarev.ru>
2023-06-27 14:50:34 +0300

    Update import handler

--- src/handlers/private/user/__init__.py
+++ src/handlers/private/user/__init__.py
@@ -1,10 +1,7 @@
 from aiogram import Router
 
-from . import start, dialogs
+from . import start
 
 router = Router()
 router.include_router(start.router)
-router.include_router(dialogs.router)
 router.include_router(start.start_menu)
-# router.include_router(test.router)
-# router.include_router(test.dialog)
