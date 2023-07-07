bf565614848b63ef0c7d843a8b5657ab436d6085
Af1S <ferma813@gmail.com>
2023-07-08 02:57:15 +0300

    reformatting

--- src/dialogs/start.py
+++ src/dialogs/start.py
@@ -1,20 +1,20 @@
-from aiogram.types import CallbackQuery
-from aiogram.types import CallbackQuery
 from aiogram_dialog import DialogManager, Dialog, Window
-from aiogram_dialog.widgets.kbd import Start, Button
+from aiogram_dialog import DialogManager, Dialog, Window
+from aiogram_dialog.widgets.kbd import Start
 from aiogram_dialog.widgets.text import Const
 
 from src.dialogs.utils.common import on_start_copy_start_data
+from src.models.personal_data import PersonalDataHandler
 from src.models.tracks import TrackHandler
-from src.models.user import UserHandler
-from src.utils.fsm import StartMenu, Listening, Library, PublicTrack, Service, ListeningNewTrack, PersonalDataFilling
+from src.utils.fsm import StartMenu, Listening, Library, PublicTrack, Service, PersonalDataFilling
 
 
 async def get_data(dialog_manager: DialogManager, **kwargs):
     data = dialog_manager.middleware_data
     user_id = data['event_from_user'].id
     library = await TrackHandler(data['engine'], data['database_logger']).has_tracks_by_tg_id(user_id)
-    user_data = not (await UserHandler(data['engine'], data['database_logger']).check_all_data_complete(user_id))
+    user_data = not (
+        await PersonalDataHandler(data['engine'], data['database_logger']).check_all_data_complete(user_id))
     tracks = await TrackHandler(data['engine'], data['database_logger']).check_chat_exists(user_id)
     return {
         "library_check": library,
