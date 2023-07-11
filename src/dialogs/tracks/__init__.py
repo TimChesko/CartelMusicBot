e77ba25a3eecd985ed8ea1d5c7d1df8a64384cc1
Af1S <ferma813@gmail.com>
2023-07-11 19:38:12 +0300

    rename track files

--- src/dialogs/tracks/__init__.py
+++ src/dialogs/tracks/__init__.py
@@ -1,11 +1,11 @@
 from aiogram import Router
 
-from . import listening, my_tracks, rejected_tracks, approved_tracks
+from . import listening, tracks_menu, rejected, approved
 
 router = Router()
 router.include_router(listening.listening_menu)
 router.include_router(listening.new_track)
 router.include_router(listening.edit_track)
-router.include_router(my_tracks.my_tracks_menu)
-router.include_router(rejected_tracks.reloading_on_listening)
-router.include_router(approved_tracks.approved_filling_data)
+router.include_router(tracks_menu.my_tracks_menu)
+router.include_router(rejected.reloading_on_listening)
+router.include_router(approved.approved_filling_data)
