ea631bd28e6a027689e799729fa13e8096e7a860
Tima <timofey.lazarev@gmail.com>
2023-07-16 10:06:05 +0300

    Delete useless states group

--- src/utils/fsm.py
+++ src/utils/fsm.py
@@ -101,38 +101,6 @@ class MyTracksRejected(StatesGroup):
     finish = State()
 
 
-class MyTracksProcessed(StatesGroup):
-    ...
-
-
-class PersonalDataFilling(StatesGroup):
-    start = State()
-    info = State()
-    firstname = State()
-    surname = State()
-    middlename = State()
-    passport_series = State()  # серия паспорта
-    passport_number = State()  # номер паспорта
-    who_issued_it = State()  # кем выдан
-    date_of_issue = State()  # когда выдан
-    unit_code = State()  # код подразделения
-    date_of_birth = State()  # дата рождения
-    place_of_birth = State()  # место рождения
-    registration_address = State()  # адрес регистрации
-    physical_inn_code = State()  # ИНН по паспорту
-
-    recipient = State()  # Получатель
-    account_code = State()  # Номер счёта
-    bik_code = State()  # БИК
-    bank_recipient = State()  # Банк получатель
-    correct_code = State()  # Корр. Счет
-    inn_code = State()  # ИНН
-    kpp_code = State()  # КПП
-    swift_code = State()  # Свифт-код
-
-    finish = State()
-
-
 # Public tracks states
 class PublicTrack(StatesGroup):
     list = State()
