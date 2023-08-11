dff7da08cc989f089104ef6e00c8b5f6651b9667
Af1S <ferma813@gmail.com>
2023-08-11 16:43:24 +0300

    metadata fix

--- src/dialogs/admin/tasks/release/menu.py
+++ src/dialogs/admin/tasks/release/menu.py
@@ -10,10 +10,14 @@ from src.utils.fsm import AdminRelease, AdminReleaseLvl1, \
 
 main = Dialog(
     Window(
-        Const("Выберите уровень"),
-        Start(Const('lvl1'), state=AdminReleaseLvl1.start, id='lvl1_start'),
-        Start(Const('lvl2'), state=AdminReleaseLvl2.start, id='lvl2_start'),
-        Start(Const('lvl3'), state=AdminReleaseLvl3.start, id='lvl3_start'),
+        Const("Тут находятся все проверки связанные с лицензионным договором\n"
+              "Они делятся на три типа:\n"
+              "1) Проверка обложки, названия альбома, списка треков и первичного лицензионного договора\n"
+              "2) Проверка подписи лицензионного договора\n"
+              "2) Проверка трек номера CDEK"),
+        Start(Const('Первичная'), state=AdminReleaseLvl1.start, id='lvl1_start'),
+        Start(Const('Подпись'), state=AdminReleaseLvl2.start, id='lvl2_start'),
+        Start(Const('Трек номер'), state=AdminReleaseLvl3.start, id='lvl3_start'),
         BTN_CANCEL_BACK,
         state=AdminRelease.menu
     )
