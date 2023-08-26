32594856f4c1b96914e69abf6f8b61c020421469
timchesko <i@tlazarev.ru>
2023-08-26 13:40:55 +0300

    Fix logger

--- src/utils/dialogs.py
+++ src/utils/dialogs.py
@@ -17,10 +17,41 @@ class AiogramDialogLogging(logging.Handler):
         new_logger = self.logger
         patterns = [
             (r"^(.*?):\s*(.*)$", ["action", "details"]),
+
             (r"(.*)\((.*)\)", ["action", "details"]),
-            (r"^(.*?) to id=(\d+) type='(.*?)' title=(.*?) username='(.*?)' first_name='(.*?)' last_name='(.*?)'",
-             ["action", "user_id", "message_type", "title", "username", "first_name", "last_name"])
+
+            (r"^(.*?) to id=(\d+) type='(\w+)' title=(\w+) username='(\w+)' first_name='(\w+)' last_name='(\w+)' "
+             r"is_forum=(\w+) photo=(\w+) active_usernames=(\w+) emoji_status_custom_emoji_id=(\w+) bio=(\w+) "
+             r"has_private_forwards=(\w+) has_restricted_voice_and_video_messages=(\w+) join_to_send_messages=(\w+) "
+             r"join_by_request=(\w+) description=(\w+) invite_link=(\w+) pinned_message=(\w+) permissions=(\w+) "
+             r"slow_mode_delay=(\w+) message_auto_delete_time=(\w+) has_aggressive_anti_spam_enabled=(\w+) "
+             r"has_hidden_members=(\w+) has_protected_content=(\w+) sticker_set_name=(\w+) "
+             r"can_set_sticker_set=(\w+) "
+             r"linked_chat_id=(\w+) location=(\w+)$",
+             ["action", "user_id", "message_type", "title", "username", "first_name", "last_name", "is_forum",
+              "photo", "active_usernames", "emoji_status_custom_emoji_id", "bio", "has_private_forwards",
+              "has_restricted_voice_and_video_messages", "join_to_send_messages", "join_by_request", "description",
+              "invite_link", "pinned_message", "permissions", "slow_mode_delay", "message_auto_delete_time",
+              "has_aggressive_anti_spam_enabled", "has_hidden_members", "has_protected_content", "sticker_set_name",
+              "can_set_sticker_set", "linked_chat_id", "location"]),
+
+            (r"^(\w+) in id=(\d+) type='(\w+)' title=(\w+) username='(\w+)' first_name='(\w+)' last_name='(\w+)' "
+             r"is_forum=(\w+) photo=(\w+) active_usernames=(\w+) emoji_status_custom_emoji_id=(\w+) bio=(\w+) "
+             r"has_private_forwards=(\w+) has_restricted_voice_and_video_messages=(\w+) join_to_send_messages=(\w+) "
+             r"join_by_request=(\w+) description=(\w+) invite_link=(\w+) pinned_message=(\w+) permissions=(\w+) "
+             r"slow_mode_delay=(\w+) message_auto_delete_time=(\w+) has_aggressive_anti_spam_enabled=(\w+) "
+             r"has_hidden_members=(\w+) has_protected_content=(\w+) sticker_set_name=(\w+) "
+             r"can_set_sticker_set=(\w+) "
+             r"linked_chat_id=(\w+) location=(\w+)$",
+             ["action", "user_id", "message_type", "title", "username", "first_name", "last_name", "is_forum",
+              "photo", "active_usernames", "emoji_status_custom_emoji_id", "bio", "has_private_forwards",
+              "has_restricted_voice_and_video_messages", "join_to_send_messages", "join_by_request", "description",
+              "invite_link", "pinned_message", "permissions", "slow_mode_delay", "message_auto_delete_time",
+              "has_aggressive_anti_spam_enabled", "has_hidden_members", "has_protected_content", "sticker_set_name",
+              "can_set_sticker_set", "linked_chat_id", "location"])
         ]
+        if self.log_message in ["", "[]", "{}"]:
+            return
         for pattern, group_names in patterns:
             match = re.match(pattern, self.log_message)
             if match:
@@ -31,7 +62,7 @@ class AiogramDialogLogging(logging.Handler):
                 new_logger.debug(action_message)
                 break
         else:
-            new_logger.error("Pattern not found for message:", self.log_message)
+            new_logger.error("Pattern not found for message: {}".format(self.log_message))
 
 
 def setup_dialog_logging(dialog_logger):
